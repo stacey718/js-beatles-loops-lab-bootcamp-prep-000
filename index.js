f35ec61de1154d77e4a11640973c836d64b52f5c
S P <stacey.cupid@gmail.com>
2018-10-22 16:20:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ function theBeatlesPlay(musicians, instruments) {
   return newArray
 }
 
+var facts = [
+    "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ];
 function johnLennonFacts(array) {
   var j = [];
   let l = 0;
