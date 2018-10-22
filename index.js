7a2b98d089fbcc8c07146b758d89cba4235687ae
S P <stacey.cupid@gmail.com>
2018-10-22 17:21:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ var b = 0;
 function iLoveTheBeatles(b) {
   var beats = [];
   b = b + 1;
-}
+  
 do {
   beats.push("I love the Beatles!"); iLoveTheBeatles(); b < 7;
 } while (b < 8);
- 
+return beats
+}
