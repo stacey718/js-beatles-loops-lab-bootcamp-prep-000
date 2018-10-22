4327bfb9119e4dc482b2104751df9ae809ec1615
S P <stacey.cupid@gmail.com>
2018-10-22 16:32:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments) {
   return newArray
 }
 
-var facts = [
+const facts = [
     "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
